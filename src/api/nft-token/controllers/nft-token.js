fe443c50f53e4a38c3b9a12c6b04143b3f627894
nickelefth <nikolas.eleuftheriou@gmail.com>
2022-03-22 13:34:24 +0000

    added cms strucutre

---
+++ src/api/nft-token/controllers/nft-token.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  nft-token controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::nft-token.nft-token');
---
+++ src/api/nft-token/routes/nft-token.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * nft-token router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::nft-token.nft-token');
