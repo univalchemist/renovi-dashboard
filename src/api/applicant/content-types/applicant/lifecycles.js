862e9bc2c127cd03953324c33d208e1b35ff4dc3
nickelefth <nikolas.eleuftheriou@gmail.com>
2022-03-23 14:46:37 +0000

    configured email with sendgrid very coolio, updated models with latest requirements, updated hooks to create approval and declined email when changing the status of the applicant

---
+++ src/api/applicant/content-types/applicant/lifecycles.js
@@ -0,0 +1,26 @@
+module.exports = {
+    // beforeCreate(event) {
+    //   console.log(event.params);
+    //   // let's do a 20% discount everytime
+    // //   event.params.data.price = event.params.data.price * 0.8;
+    // },
+
+    async beforeUpdate(event) {
+        const {full_name, status, email } = event.params.data;
+
+        if (status === 'approved') {
+          await strapi.plugins['email'].services.email.send({
+            to: email,
+            subject: 'Renovi Application Approved',
+            html: `Hi ${full_name} <br/><br/>Your application has been approved<br/><br/>Best,<br/>Renovi Team`,
+          });
+        } else if (status === 'declined') {
+          await strapi.plugins['email'].services.email.send({
+            to: email,
+            subject: 'Renovi Application Declined',
+            html: `Hi ${full_name} <br/><br/>Your application has been declined<br/><br/>Best,<br/>Renovi Team`,
+          });
+        }
+      },
+
+  };
--- src/api/applicant/controllers/applicant.js
+++ src/api/applicant/controllers/applicant.js
@@ -6,4 +6,24 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::applicant.applicant');
+module.exports = createCoreController('api::applicant.applicant', ({ strapi }) =>  ({
+
+    async create(ctx) {
+        // some logic here
+        const response = await super.create(ctx);
+        console.log("ctx :: ", ctx);
+        // some more logic
+      
+        return response;
+      },
+
+      async update(ctx) {
+        // some logic here
+        const response = await super.update(ctx);
+        console.log("ctx :: ", ctx);
+        // some more logic
+      
+        return response;
+      }
+
+  }));
