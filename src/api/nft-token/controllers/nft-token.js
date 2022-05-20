1ebcfcc78f588a059e0304b8daaafdec5b2c10d6
nickelefth <nikolas.eleuftheriou@gmail.com>
2022-05-20 12:58:09 +0100

    update the schema

--- src/api/nft-token/controllers/nft-token.js
+++ src/api/nft-token/controllers/nft-token.js
@@ -7,29 +7,4 @@
 const { createCoreController } = require('@strapi/strapi').factories;
 
 
-module.exports = createCoreController('api::nft-token.nft-token', ({ strapi }) =>  ({
-
-    async find(ctx) {
-        const { query } = ctx;
-
-        const entity = await strapi.entityService.findMany('api::nft-token.nft-token', {
-            ...query,
-            populate: {
-                owner: {
-                    populate: {
-                        avatar: true
-                    }
-                },
-                applicant: {
-                    populate: {
-                        avatar: true
-                    }
-                },
-            },
-        });
-        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-
-        return this.transformResponse(sanitizedEntity);
-      }
-
-  }));
+module.exports = createCoreController('api::nft-token.nft-token', ({ strapi }) =>  ({}));
